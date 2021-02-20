aa72dea1891d32e0e7db70113a6cc0a12b91ee9e
pororodroid <pikachu.972011@gmail.com>
2021-02-20 21:24:03 +0800

    create new file

---
+++ MerraylenPacheco-AsawaniLeviAckerman/index.ts
@@ -0,0 +1,42 @@
+export{}
+let message = 'Welcome back, Levi';
+console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'Codeevolution';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Merraylen';
+
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+enum Color {Red = 5, Green, Blue};
+
+let c: Color = Color.Green;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Merraylen';
+
+let myVariable: any = 10;
+
+console.log(myVariable.name);
+myVariable();
+myVariable.toUpperCase();
